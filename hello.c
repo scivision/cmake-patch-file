494b371d0ea23f749d89f52a9cd8d920a4a3c124
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-16 11:46:46 -0500

    init

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "value.h"
+#include "other.h"
+
+int main(void){
+  assert(other == 3);
+
+  printf("Returning %d", RET);
+  assert(RET == 0);
+  return 0;
+}
