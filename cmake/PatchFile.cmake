3a5c478a83b26d622a8f403c830efef31b53d154
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-15 20:58:12 -0400

    on Windows, prioritize Git Patch

--- cmake/PatchFile.cmake
+++ cmake/PatchFile.cmake
@@ -1,23 +1,22 @@
 # use GNU Patch from any platform
 
-find_program(PATCH NAMES patch)
-
-if(WIN32 AND NOT PATCH)
+if(WIN32)
+  # prioritize Git Patch on Windows as other Patches may be very old and incompatible.
   find_package(Git)
   if(Git_FOUND)
     get_filename_component(GIT_DIR ${GIT_EXECUTABLE} DIRECTORY)
     get_filename_component(GIT_DIR ${GIT_DIR} DIRECTORY)
-
-    find_program(PATCH
-    NAMES patch
-    HINTS ${GIT_DIR}
-    PATH_SUFFIXES usr/bin
-    )
   endif()
 endif()
 
+find_program(PATCH
+NAMES patch
+HINTS ${GIT_DIR}
+PATH_SUFFIXES usr/bin
+)
+
 if(NOT PATCH)
-  message(FATAL_ERROR "Did not find GNU PATCH")
+  message(FATAL_ERROR "Did not find GNU Patch")
 endif()
 
 execute_process(COMMAND ${PATCH} ${in_file} --input=${patch_file} --output=${out_file} --ignore-whitespace
